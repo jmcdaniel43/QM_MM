47f1b876a0e330977f1889944d73955492764c71
John Patrick Pederson Jr <jpederson6@login-phoenix-3.pace.gatech.edu>
2022-04-12 09:34:11 -0400

    Corrected documentation

--- docs/html/search/all_67.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['generate_5fgeometry',['generate_geometry',['../classqm__mm_1_1psi4__interface_1_1psi4__interface.html#a660fd7e726174d2bba77c9e162e620ea',1,'qm_mm::psi4_interface::psi4_interface']]],
-  ['generate_5fpme_5fexclusions',['generate_pme_exclusions',['../classqm__mm_1_1psi4__interface_1_1psi4__interface.html#a75c8238500fa9969d6b6f0495e2f879f',1,'qm_mm::psi4_interface::psi4_interface']]],
-  ['generate_5fpme_5fxyz',['generate_pme_xyz',['../classqm__mm_1_1psi4__interface_1_1psi4__interface.html#aee7153d3d7f9c5d3fc412331f87d3554',1,'qm_mm::psi4_interface::psi4_interface']]]
-];
--- docs/html/search/all_6f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['openmm_5finterface',['openmm_interface',['../classqm__mm_1_1openmm__interface_1_1openmm__interface.html',1,'qm_mm::openmm_interface']]]
-];
